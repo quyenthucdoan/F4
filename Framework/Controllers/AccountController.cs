44b907e63d2ff178d516477f74c6652982faa4bc
NguyenDucAnhvt <74885211+NguyenDucAnhvt@users.noreply.github.com>
2020-11-25 19:29:46 +0700

    giao dien (navigation bar + welcome)

---
+++ Framework/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Framework.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Registration POST action
+
+
+        //Verify Email
+
+
+        //Verify Email LINK
+
+
+        //Login
+
+
+        //Login POST
+
+
+        //Logout
+    }
+}
